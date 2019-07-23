968db36b6440c8204a5b4e32e878cd6a1b89ac6d
mffabrigas <migfabrigas@gmail.com>
2019-07-23 11:40:19 -0700

    added base handlers

--- main.py
+++ main.py
@@ -2,9 +2,31 @@ import webapp2
 import jinja2
 import os
 
-def MainHandler(webapp2.RequestHandler):
-    print("Hello World!")
+jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True
+)
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        print("MainHandler works!")
+
+class LoginHandler(webapp2.RequestHandler):
+    def get(self):
+        print("LoginHandler works!")
+
+class InputHandler(webapp2.RequestHandler):
+    def get(self):
+        print("InputHandler works!")
+
+class ProfileHandler(webapp2.RequestHandler):
+    def get(self):
+        print("ProfileHandler works!")
 
 app = webapp2.WSGIApplication([
-    ("/", MainHandler)
+    ("/", MainHandler),
+    ("/login", LoginHandler),
+    ("/input", InputHandler),
+    ("/profile", ProfileHandler),
 ], debug=True)
