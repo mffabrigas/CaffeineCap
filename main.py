61f3fde2b258327be1c9eb9f105b21a866961ae3
mffabrigas <migfabrigas@gmail.com>
2019-07-23 11:52:14 -0700

    Added post method to input handler to take in user input

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ jinja_env = jinja2.Environment(
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+
         print("MainHandler works!")
 
 class LoginHandler(webapp2.RequestHandler):
@@ -19,6 +20,15 @@ class LoginHandler(webapp2.RequestHandler):
 class InputHandler(webapp2.RequestHandler):
     def get(self):
         print("InputHandler works!")
+        input_template = jinja_env.get_template("templates/input.html")
+        self.response.write(input_template.render())
+
+    def post(self):
+        coffee_type = self.request.get("coffee_type")
+        caffeine_amount = self.request.get("caffeine_amount")
+
+        print(coffee_type)
+        print(caffeine_amount)
 
 class ProfileHandler(webapp2.RequestHandler):
     def get(self):
