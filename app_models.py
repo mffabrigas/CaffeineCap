084088641c340d40a2f87a07496f3ff83d5b8d9e
mffabrigas <migfabrigas@gmail.com>
2019-07-26 15:34:41 -0700

    Made coffee entries specific to user and removed latency between put and fetch coffee entry to and from datastore

--- app_models.py
+++ app_models.py
@@ -7,12 +7,12 @@ class Coffee_Entry(ndb.Model):
 
 #One to Many
 class User(ndb.Model):
-    user_id = ndb.IntegerProperty(required=True)
+    user_id = ndb.StringProperty(required=True)
     coffee_entries = ndb.KeyProperty(Coffee_Entry, repeated=True)
 
     @classmethod
     def get_by_user(cls, user):
         return cls.query().filter(cls.user_id == user.user_id()).get()
 
-    def get_coffee_entries(cls):
-        return cls.query()
+    def get_coffee_entries():
+        return
